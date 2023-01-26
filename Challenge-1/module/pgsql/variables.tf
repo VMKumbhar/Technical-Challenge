cf542ed477dfa13a045f89f1ca74ba3a95d82575
VMKumbhar <46930216+VMKumbhar@users.noreply.github.com>
2023-01-27 00:42:48 +0530

    variable.tf

---
+++ Challenge-1/module/pgsql/variables.tf
@@ -0,0 +1,12 @@
+variable "project-name" {}
+variable "vpc-id" {}
+#variable "shared-project" {}
+variable "db-name" {}
+
+variable "iam-roles" {
+  description = "List of IAM resources to allow access to the pgsql db"
+  type        = list(string)
+  default     = [
+    "roles/cloudsql.client"
+    ]
+}
