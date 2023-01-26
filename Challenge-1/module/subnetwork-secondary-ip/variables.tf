ae1c8d6e3324b1e596093b0e80d5c7c5a0746618
VMKumbhar <46930216+VMKumbhar@users.noreply.github.com>
2023-01-27 00:47:15 +0530

    Add files via upload

---
+++ Challenge-1/module/subnetwork-secondary-ip/variables.tf
@@ -0,0 +1,10 @@
+variable "shared-project" {}
+variable "subnet-vpc" {}
+variable "subnet-name" {}
+variable "subnet-ip" {}
+variable "region" {}
+variable "gke-pod-ip-address-name" {}
+variable "gke-pod-ip-address" {}
+variable "gke-svc-ip-address-name" {}
+variable "gke-svc-ip-address" {}
+
